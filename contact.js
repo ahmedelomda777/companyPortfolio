b032e1e4db7258cf914095789f95cf78087c7b13
ahmed abbas <ahmedabs777@gmail.com>
2023-05-22 17:34:07 +0200

    updated changes

--- contact.js
+++ contact.js
@@ -1,3 +1,4 @@
+
 let nav= document.querySelector("nav")
 window.addEventListener("scroll",()=>{
    nav.classList.toggle("nav-scroll", window.scrollY>100)
@@ -37,10 +38,10 @@ const pass = document.querySelector("#inputPassword4")
 const mess = document.querySelector("#message")
 
 
-let validateName="false"
-let validateEmail="false"
-let validatePass="false"
-let validateMess="false"
+let validateName=false
+let validateEmail=false
+let validatePass=false
+let validateMess=false
 
 
 namee.onkeyup= ()=>{
@@ -51,7 +52,7 @@ namee.onkeyup= ()=>{
   else{
     p1.style.display="none"
     namee.style.border="none"
-    validateName="true"
+    validateName=true
 
   }
 }
@@ -75,7 +76,7 @@ email.onkeyup = ()=>{
       p2.style.display="none"
       email.style.border="1px solid #dfe3ee"
       p2.textContent="*...@....com"
-      validateEmail="true"
+      validateEmail=true
 
   }
 }
@@ -100,7 +101,7 @@ pass.onkeyup= ()=>{
       p3.style.display="none"
       pass.style.border="1px solid #dfe3ee"
       p3.textContent="*make it strong by adding litters and numbers and marks"
-      validatePass="true"
+      validatePass=true
   }
 }
 pass.onblur = ()=>{
@@ -116,43 +117,46 @@ mess.onkeyup= ()=>{
   else{
     p4.style.display="none"
     mess.style.border="none"
-    validateMess="true"
+    validateMess=true
 
   }
 }
+console.log(validateEmail);
+console.log(validateMess);
+console.log(validateName);
+console.log(validatePass);
 
 
-document.forms[0].onsubmit= ()=>{
-if (validateName==="false"||validateEmail==="false"||validatePass==="false"||validateMess==="false"){
- event.preventDefault()
+ function validation(e){
 
- if(validateName==="false"){
-  p1.style.display="block"
+ if(validateName === false || validateEmail=== false || validatePass=== false || validateMess=== false ){
+  e.preventDefault()
+
+ if(validateName=== false){
+  p1.style.display= "block"
   namee.style.border="red solid 1px"
 
  }
- else if(validateEmail==="false"){
+ else if(validateEmail=== false ){
   p2.style.display="block"
   email.style.border="red solid 1px"
   p1.textContent="please fill up your email\">9\""
 
  }
  
- else if(validatePass==="false"){
+ else if(validatePass=== false){
   p3.style.display="block"
   pass.style.border="red solid 1px"
   p3.textContent="please fill up your pass\">5\""
 
  }
- else{
+ else if(validateMess=== false){
   p4.style.display="block"
   mess.style.border="red solid 1px"
 
- }}
-
-else {
-  function sendEmail()
+ }
 }
+
 }
 
 
@@ -170,17 +174,4 @@ else {
 
 
 
-// let drk=document.querySelector("#black")
-// drk.addEventListener("click",()=>{
-//   document.body.classList.add("dark")
-//   document.querySelector(".Recent").classList.add("dark-text")
-//   document.querySelector("fieldset").classList.add("dark-grey")
-//   document.querySelector("#black").classList.add("black-dark")
-//   if(drk.onclick==="true"){
-//     document.body.classList.remove("dark")
-//     document.querySelector(".Recent").classList.remove("dark-text")
-//     document.querySelector("fieldset").classList.remove("dark-grey")
-//     document.querySelector("#black").classList.remove("black-dark")
-// }
 
-// }) 
